563b008516ad0a42b285a384e8f1a05e138442d8
Jenny Fong <jennyfong1015@gmail.com>
2021-02-07 22:28:07 -0500

    Display list and allow shortened links to be removed

---
+++ src/components/UrlList/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { removeLink } from "../../api";
+import useApiError from "../../hooks/useApiError";
+
+function UrlList({ links, onRemove }) {
+  const { handleApiCall } = useApiError();
+
+  async function handleRemove(slug) {
+    await handleApiCall(() => removeLink(slug));
+    // always remove slug even 404
+    // (deleted on another tab but current tab has stale data)
+    onRemove(slug);
+  }
+
+  return (
+    <ul>
+      {links.map((link) => {
+        return (
+          <li key={link.slug}>
+            <div>
+              {link.url}
+              {link.short_url}
+
+              <div>
+                <button type="button" onClick={() => handleRemove(link.slug)}>
+                  Remove
+                </button>
+              </div>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default UrlList;
